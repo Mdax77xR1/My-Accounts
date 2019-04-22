804c2b229977102ec09e7b5db0d660e493dfce11
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2019-04-22 14:11:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,40 +3,15 @@ const fs = require("fs");
 const jimp = require('jimp');
 var prefix = "$"
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity("",{type: 'watching'})
-  console.log('xR1')
-  console.log('MdAx77x')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('S.P.z')
-  console.log('ٍ :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-
 
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Welcome Mdax`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
-    console.log(`----------------`);
+   console.log(`ـــــــــــــــــــ`);
+      console.log(`Welcome MdAx77x | Welcome To Evel Gates`);
+        console.log(`ــــــــــــــــ`);
+      console.log(`ON ${client.guilds.size} Server`);
+console.log(`This Code Was Made By : Mdax77x`);   
+console.log(`MdAx77x CopyRight `);
+ console.log(`ــــــــــــــــ`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`م رخصنا رفيق ولآ ظلمنآ عدو لآكن كلن بفعله يحدد مكانتة #☠️🔕`)
 client.user.setStatus("offline")
