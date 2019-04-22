9f9ace8f4153567779a9d3b057fc939e5d0229d0
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2019-04-22 12:28:38 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,48 @@
+client.user.setActivity(argresult , {type:'LISTENING'});
+const Discord = require('discord.js');
+const fs = require("fs"); 
+const jimp = require('jimp');
+var prefix = "$"
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setActivity("",{type: 'watching'})
+  console.log('xR1')
+  console.log('MdAx77x')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('S.P.z')
+  console.log('ٍ :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Welcome Mdax`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`م رخصنا رفيق ولآ ظلمنآ عدو لآكن كلن بفعله يحدد مكانتة #☠️🔕`)
+client.user.setStatus("offline")
+ 
+});
+
+
+client.login(process.env.BOT_TOKEN);
