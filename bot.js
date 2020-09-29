52f7cc33ccc37fa998af40f434e5063b964e7ffb
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2020-09-29 13:45:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,17 +4,37 @@ const jimp = require('jimp');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Welcome Mdax`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
-    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Falconse Roleplay | https://discord.gg/Rsgyruy`)
-client.user.setStatus("dnd")
- 
+ client.user.setActivity("",{type: 'DND'})
+  console.log('The Prince : MdAx77x')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('Created by: SPz / xR1.')
+  console.log('with some help: .Manchez')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' HeRx Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Welcome to evil gates.`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} servers.`);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`); 
+});
 
 
 client.login(process.env.BOT_TOKEN);
