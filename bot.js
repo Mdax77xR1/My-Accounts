acf55725e116f36e16c6f7b7fae2816549500d59
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2020-09-29 13:15:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,38 +13,9 @@ console.log(`This Code Was Made By : Mdax77x`);
 console.log(`MdAx77x CopyRight `);
  console.log(`ــــــــــــــــ`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`م رخصنا رفيق ولآ ظلمنآ عدو لآكن كلن بفعله يحدد مكانتة #☠️🔕`)
-client.user.setStatus("offline")
+client.user.setStatus("DND")
  
 });
 
 
-client.on('message', message => {
-    if(!message.channel.guild) return;
-    if(message.content.startsWith('Mping')) { // حقوق مداكس
-        if (message.author.bot) return;
-        if(!message.channel.guild) return;
-        var Bping =`${Math.round(client.ping)}` // Mdax77x CopyRight | Toxic Codes
-                const E1ping = new Discord.RichEmbed()
-        .setTitle('ــــــــــــــــــــــــــــــ')
-        .addField(`**BOT Ping Is** :__${Bping}📶__`,"ــــــــــــــــــــــــــــــ")
-        .setFooter(`Requested by | ${message.author.tag}`) // حقوق مداكس
-        .setColor('RANDOM')
-        message.channel.send(E1ping);
-    }
-});
-client.on('message',message =>{
-    if(!message.channel.guild) return;
-if(message.content =='^members')
-var E2Mdax = new Discord.RichEmbed()
-
-.setTitle('==========🌷| Members info==========')
-.addField('** Members count👥.:**',`__** [ ${message.guild.memberCount} ]**__`,true)
-.addField('📗|online',` ${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
-.addField('📓| offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
-.setFooter(`Requested By | ${message.author.tag}`)
-.addField('**==============**',true)
-.setColor('RANDOM')
-message.channel.send(E2Mdax);
-});
 client.login(process.env.BOT_TOKEN);
