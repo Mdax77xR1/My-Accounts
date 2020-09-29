0b2d848457e04541a27f2f05f1fa1d96d0d55d4a
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2020-09-29 13:40:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,17 +4,17 @@ const jimp = require('jimp');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-   console.log(`ـــــــــــــــــــ`);
-      console.log(`Welcome MdAx77x | Welcome To Evel Gates`);
-        console.log(`ــــــــــــــــ`);
-      console.log(`ON ${client.guilds.size} Server`);
-console.log(`This Code Was Made By : Mdax77x`);   
-console.log(`MdAx77x CopyRight `);
- console.log(`ــــــــــــــــ`);
+   console.log(`----------------`);
+      console.log(`Welcome Mdax`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Falconse Roleplay | https://discord.gg/Rsgyruy`)
 client.user.setStatus("dnd")
  
 });
+
 
 
 client.login(process.env.BOT_TOKEN);
