07af7925d8d9e2c935503acd69e794e246ccaa57
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2020-11-26 12:32:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,4 +29,49 @@ client.on('ready', () => {
 });
 
 
+
+
+
+ client.on('message', message => {    
+              if(!message.channel.guild) return;
+    if(message.content.startsWith('هأي1')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "xR1 Server";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+   
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+   
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+ reaction1.on("collect", r => {
+    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+ 
+  var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('حـياكم فـ مجتمع فـالكونز')
+       .addField('الرسالة', args)
+       .setFooter("أول 5 أشخاص يـدخلون سيرفر الـرولي بـلاي لهم جوائز.");
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
+
+
+
 client.login(process.env.BOT_TOKEN);
