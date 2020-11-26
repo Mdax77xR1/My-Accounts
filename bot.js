d44b4ddd8f4744f0521305530fb882f32300a9a5
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2020-11-26 12:51:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require("fs"); 
 const jimp = require('jimp');
 const client = new Discord.Client();
-const perfix = "$"
+const prefix = "$"
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
  client.user.setActivity("F.L RP https://discord.gg/Rsgyruy",{type: 'dnd'})
